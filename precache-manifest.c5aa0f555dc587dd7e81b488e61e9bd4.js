238877411da85d76e5cb60ffd8bb9b475a5b3ade
Damon Anthony <damonanthony125@gmail.com>
2020-07-07 13:19:00 +0200

    Updates

---
+++ precache-manifest.c5aa0f555dc587dd7e81b488e61e9bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98c3ef6956bf4c6e5584d98cf80a5d7",
+    "url": "/ContactKeeper/index.html"
+  },
+  {
+    "revision": "2cce4185b4746ee7b795",
+    "url": "/ContactKeeper/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "c86bdfc899f4fa06b1d9",
+    "url": "/ContactKeeper/static/js/2.6406c3ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ContactKeeper/static/js/2.6406c3ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cce4185b4746ee7b795",
+    "url": "/ContactKeeper/static/js/main.e822d980.chunk.js"
+  },
+  {
+    "revision": "579a7023732c615179d0",
+    "url": "/ContactKeeper/static/js/runtime-main.3043890a.js"
+  }
+]);
